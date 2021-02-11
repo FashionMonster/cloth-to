3830a968efba5c7445faa2e654f94c79b68d5b45
Yusuke Hirooka <yhirookajpn@gmail.com>
2021-02-11 17:11:29 +0900

    sequalize try

--- pages/api/getContribution.js
+++ pages/api/getContribution.js
@@ -1,9 +1,23 @@
-let list = ["Item 1", "Item 2", "Item 3"];
+const models = require("../../models");
 
+// let list = ["Item 1", "Item 2", "Item 3"];
+// export default function handler(req, res) {
+//   if (req.query.reqData !== "") {
+//     list.push(req.query.reqData);
+//   }
+//   console.log(list);
+//   res.json({ resData: list });
+// }
+
+let list = [];
 export default function handler(req, res) {
-  if (req.query.reqData !== "") {
-    list.push(req.query.reqData);
+  try {
+    models.User.findAll().then(function (results) {
+      console.log(results);
+    });
+  } catch (e) {
+    console.log("ERROR!", e);
   }
-  console.log(list);
+
   res.json({ resData: list });
 }
